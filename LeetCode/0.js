5c4d75345f06cb4c85fb037944ae0ed5461abd5f
xuetengfei <workingxue@gmail.com>
2022-03-15 16:43:41 +0800

    2022-03-15 16:43

--- LeetCode/0.js
+++
@@ -1,8 +0,0 @@
-const nums = [1, 2, 3, 2, 2, 2, 5, 4, 2];
-
-function fn(arr) {
-  return;
-}
-
-const r = fn(nums);
-console.log('r', r);
--- LeetCode/tempCodeRunnerFile.js
+++
@@ -1,5 +0,0 @@
-console.log(fn('+100'));
-// console.log(fn('5e2'));
-// console.log(fn('-123'));
-// console.log(fn('2.1416'));
-// console.log(fn('-1E-16'));
